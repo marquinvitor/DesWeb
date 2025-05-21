a5398e96d481759a65fdb2730c44cccbce60afad
Marcos Vitor <marcosvitorsouzafreire@gmail.com>
2025-05-20 21:28:49 -0300

    rename de atvs antigas e nova atividade do dia 20/05

---
+++ ATV_20-05-25/script.js
@@ -0,0 +1,4 @@
+document.getElementById("mudar-tema").addEventListener("click", function () {
+  // Alterna a classe "dark-mode" no <body> (liga ou desliga)
+  document.body.classList.toggle("dark-mode");
+});
